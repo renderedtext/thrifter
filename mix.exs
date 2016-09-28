29207d1e2ba108b50103aaa66243c9b0077cba1b
Predrag Rakic <prakic@renderedtext.com>
2016-09-28 22:18:59 +0200

    Add riffed dependancy

--- mix.exs
+++ mix.exs
@@ -10,23 +10,14 @@ defmodule Thrifter.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:riffed, git: "git@github.com:renderedtext/riffed",
+        ref: "origin/master"},
+    ]
   end
 end
