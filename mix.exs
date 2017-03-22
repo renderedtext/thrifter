d66dc0eb96fdb159ac4509f8733ce2a25f200cd0
Predrag Rakic <prakic@renderedtext.com>
2017-03-22 08:46:20 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Thrifter.Mixfile do
 
   def project do
     [app: :thrifter,
-     version: "0.0.1",
+     version: "1.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
