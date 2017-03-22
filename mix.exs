9c00cf28d92ae62fa043bcd45b7d22ef2db51454
Predrag Rakic <prakic@renderedtext.com>
2017-03-22 09:38:03 +0100

    synchronize repo and module name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Thrifter.Mixfile do
 
   def project do
     [app: :thrifter,
-     version: "1.0.1",
+     version: "1.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
